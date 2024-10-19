5130575409dcded226570b99fd190b7223a86046
Azat Azamat <azatkariuly@Azats-MacBook-Pro.local>
2024-10-19 11:33:07 +0900

    added express library

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-import fs from 'fs';
+import express, { Request, Response } from  'express';
+import 'dotenv/config';
+
+const port = 3000
+
+const app = express()
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
