63dd188364694b77b577e7d9bbdab2a8e51287ae
Azat Azamat <azatkariuly@Azats-MacBook-Pro.local>
2024-10-19 12:09:57 +0900

    update: routes and controllers

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from  'express';
 import 'dotenv/config';
 
+import testRoute from './routes/test.js'
+
 const port = 3000
 
 const app = express()
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+app.use('/test', testRoute);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
